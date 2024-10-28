5ccb084337b3e208b16c4afca16d547dccb74b26
Egbert Broerse <dcc2@ebroerse.nl>
2024-10-28 18:48:05 +0100

    main

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -1,12 +1,8 @@
-// import './assets/main.css'
-
 import { createApp } from "vue";
 import App from "./App.vue";
 
 import { createI18n } from "vue-i18n";
 import VueResizeObserver from "vue-resize-observer";
-
-createApp(App).mount("#app");
 
 const i18n = createI18n({
   locale: "en",
